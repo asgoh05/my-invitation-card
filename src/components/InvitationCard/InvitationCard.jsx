8fa053c343d422917e9eb1c76596ccf9755a4eb0
asgoh05 <asgoh05@gmail.com>
2022-12-31 21:47:46 +0900

    add calendar

--- src/components/InvitationCard/InvitationCard.jsx
+++ src/components/InvitationCard/InvitationCard.jsx
@@ -3,6 +3,7 @@ import MainPage from "../MainPage/MainPage";
 import GalleryPage from "../GalleryPage/GalleryPage";
 import MessagePage from "../MessagePage/MessagePage";
 import MapPage from "../MapPage/MapPage";
+import CalendarPage from "../CalendarPage/CalendarPage";
 
 export default function InvitationCard({ images }) {
   return (
@@ -10,6 +11,7 @@ export default function InvitationCard({ images }) {
       <MainPage />
       <GalleryPage images={images} />
       <MessagePage />
+      <CalendarPage />
       <MapPage />
     </>
   );
