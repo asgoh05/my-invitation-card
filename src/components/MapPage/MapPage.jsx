9329fac0894c1227122b5a9bb977aa1bb62a3e70
asgoh05 <asgoh05@gmail.com>
2022-12-31 16:33:34 +0900

    replace kakao map to google map

--- src/components/MapPage/MapPage.jsx
+++ src/components/MapPage/MapPage.jsx
@@ -1,44 +1,20 @@
 import React from "react";
 import style from "./MapPage.module.css";
-import { useInjectKakaoMapApi, Map, MapMarker } from "react-kakao-maps-sdk";
 
 export default function MapPage() {
-  useInjectKakaoMapApi({
-    appkey: process.env.REACT_APP_KAKAO_KEY,
-  });
-
-  const position = {
-    lat: 37.3828803,
-    lng: 127.091781,
-  };
-
   return (
     <div className={style.container}>
       <h1 data-aos="fade-up" ata-aos-duration="3000">
         오시는 길
       </h1>
-      <Map
+      <embed
+        className={style.map}
         data-aos="fade-up"
         ata-aos-duration="3000"
-        center={position}
-        style={{
-          // 지도의 크기
-          width: "90%",
-          height: "40vh",
-        }}
-        level={3} // 지도의 확대 레벨
-      >
-        <MapMarker // 인포윈도우를 생성하고 지도에 표시합니다
-          position={position}
-        >
-          <div className={style.tag}>
-            <div className={style.tag_location}>하누소더힐</div>
-            <div className={style.tag_link}>
-              <a href="http://kko.to/DFGRyB3kyv">열기</a>
-            </div>
-          </div>
-        </MapMarker>
-      </Map>
+        src={
+          "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3170.29922445904!2d127.08977855068387!3d37.382755342290835!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x357b585c9382c72b%3A0xcb2a2b6905978d03!2z7ZWY64iE7IaM642U7Z6Q!5e0!3m2!1sko!2skr!4v1672471415955!5m2!1sko!2skr"
+        }
+      ></embed>
       <p className={style.address} data-aos="fade-up" ata-aos-duration="3000">
         경기 성남시 분당구 판교백현로 65 <br />
         (우)13539 지번백현동 500-9
